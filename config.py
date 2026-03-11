0d4990ad22db82f8ca70f34a5bab9e13cb873ded
Habisola <akinkuolie83@gmail.com>
2026-03-11 18:22:08 +0000

    setup: db connection, schema, config and gitignore

---
+++ config.py
@@ -0,0 +1,4 @@
+DB_HOST = "localhost"
+DB_USER = "root"
+DB_PASSWORD = "Godisfaithful2000@"
+DB_NAME = "saas_analytics"
---
+++ test_imports.py
@@ -0,0 +1,6 @@
+
+import faker
+import pymysql
+import pandas
+
+print("All good!")
